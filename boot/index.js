606cc79cfc0cc713b230d9c2530ffbfcc02bd5ff
yanming <yanming-sohu@sohu.com>
2019-03-05 19:43:44 +0800

    Signed-off-by: yanming <yanming-sohu@sohu.com>

--- boot/index.js
+++ boot/index.js
@@ -7,4 +7,27 @@ const main = config['main-script'];
 global.screen = config.screen;
 global.game = config.game || {};
 
+
 vm.loadModule(main);
+
+
+//
+// ! 未安装到模块, 新线程, 新模块都需要安装
+//
+function install_console() {
+  [
+    'debug', 'info', 'log', 'warn', 'error', 'fatal', 'line'
+  ].forEach(function(n) {
+    console[n] = _wrap_cos(console[n]);
+  });
+    
+  function _wrap_cos(sfn) {
+    return function() {
+      let out = [];
+      for (let i=0, len=arguments.length; i<len; ++i) {
+        out.push(JSON.stringify(arguments[i]));
+      }
+      sfn.apply(console, out);
+    }
+  }
+}
