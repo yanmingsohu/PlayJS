4157b683fb504a78e0ba413f569a1aa9fb79510a
yanming <yanming-sohu@sohu.com>
2019-04-27 14:36:24 +0800

    删除 demo/art 目录
    Signed-off-by: yanming <yanming-sohu@sohu.com>

--- art/README.md
+++
@@ -1,3 +0,0 @@
-# Model
-
-`ply` model from `https://github.com/ephtracy/voxel-model`
--- demo/thread.js
+++
@@ -1,9 +0,0 @@
-export default {}
-
-//
-// 启动线程
-//
-console.log("启动 3 个线程");
-thread.run('./demo/pi.js');
-thread.run('./demo/pi.js');
-thread.run('./demo/pi.js');
